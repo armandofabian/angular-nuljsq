7850ccf9dc359f8dfe2a504db6860d1dbbfdf348
armandofabian <armando.toxico@gmail.com>
2020-07-15 01:41:17 -0500

    3 files modified, 7 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
+
+
 export class CartService {
   items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -18,6 +25,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 
 }
